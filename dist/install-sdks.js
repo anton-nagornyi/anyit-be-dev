d606c20134fb30e8b26cc5411a697241dbc9287d
anton <anton.nagornyi@gmail.com>
2023-12-17 12:30:59 +0200

    fix: remove logging

--- dist/install-sdks.js
+++ dist/install-sdks.js
@@ -17,8 +17,6 @@ function copyDir(src, dest) {
   fs.mkdirSync(dest, { recursive: true });
   const entries = fs.readdirSync(src, { withFileTypes: true });
 
-  fs.appendFileSync(process.env.PROJECT_CWD + '/.test.log', `Copy dir, ${src} -> ${dest}\n`, 'utf-8');
-
   for (const entry of entries) {
     const srcPath = path.join(src, entry.name);
     const destPath = path.join(dest, entry.name);
