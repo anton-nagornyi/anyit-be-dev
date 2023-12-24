6926a77db310033a972c8ba946fa4d29276163ef
anton <anton.nagornyi@gmail.com>
2023-12-24 20:31:16 +0200

    fix: types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,66 @@
-/// <reference types="node" />
-/// <reference types="jest" />
+// NOTE: These definitions support NodeJS and TypeScript 4.9+.
+
+// Reference required types from the default lib:
+/// <reference lib="es2020" />
+/// <reference lib="esnext.asynciterable" />
+/// <reference lib="esnext.intl" />
+/// <reference lib="esnext.bigint" />
+
+// Base definitions for all NodeJS modules that are not specific to any version of TypeScript:
+/// <reference path="types/node/assert.d.ts" />
+/// <reference path="types/node/assert/strict.d.ts" />
+/// <reference path="types/node/globals.d.ts" />
+/// <reference path="types/node/async_hooks.d.ts" />
+/// <reference path="types/node/buffer.d.ts" />
+/// <reference path="types/node/child_process.d.ts" />
+/// <reference path="types/node/cluster.d.ts" />
+/// <reference path="types/node/console.d.ts" />
+/// <reference path="types/node/constants.d.ts" />
+/// <reference path="types/node/crypto.d.ts" />
+/// <reference path="types/node/dgram.d.ts" />
+/// <reference path="types/node/diagnostics_channel.d.ts" />
+/// <reference path="types/node/dns.d.ts" />
+/// <reference path="types/node/dns/promises.d.ts" />
+/// <reference path="types/node/dns/promises.d.ts" />
+/// <reference path="types/node/domain.d.ts" />
+/// <reference path="types/node/dom-events.d.ts" />
+/// <reference path="types/node/events.d.ts" />
+/// <reference path="types/node/fs.d.ts" />
+/// <reference path="types/node/fs/promises.d.ts" />
+/// <reference path="types/node/http.d.ts" />
+/// <reference path="types/node/http2.d.ts" />
+/// <reference path="types/node/https.d.ts" />
+/// <reference path="types/node/inspector.d.ts" />
+/// <reference path="types/node/module.d.ts" />
+/// <reference path="types/node/net.d.ts" />
+/// <reference path="types/node/os.d.ts" />
+/// <reference path="types/node/path.d.ts" />
+/// <reference path="types/node/perf_hooks.d.ts" />
+/// <reference path="types/node/process.d.ts" />
+/// <reference path="types/node/punycode.d.ts" />
+/// <reference path="types/node/querystring.d.ts" />
+/// <reference path="types/node/readline.d.ts" />
+/// <reference path="types/node/readline/promises.d.ts" />
+/// <reference path="types/node/repl.d.ts" />
+/// <reference path="types/node/stream.d.ts" />
+/// <reference path="types/node/stream/promises.d.ts" />
+/// <reference path="types/node/stream/consumers.d.ts" />
+/// <reference path="types/node/stream/web.d.ts" />
+/// <reference path="types/node/string_decoder.d.ts" />
+/// <reference path="types/node/test.d.ts" />
+/// <reference path="types/node/timers.d.ts" />
+/// <reference path="types/node/timers/promises.d.ts" />
+/// <reference path="types/node/tls.d.ts" />
+/// <reference path="types/node/trace_events.d.ts" />
+/// <reference path="types/node/tty.d.ts" />
+/// <reference path="types/node/url.d.ts" />
+/// <reference path="types/node/util.d.ts" />
+/// <reference path="types/node/v8.d.ts" />
+/// <reference path="types/node/vm.d.ts" />
+/// <reference path="types/node/wasi.d.ts" />
+/// <reference path="types/node/worker_threads.d.ts" />
+/// <reference path="types/node/zlib.d.ts" />
+
+/// <reference path="types/node/globals.global.d.ts" />
+
+/// <reference path="types/jest.d.ts" />
