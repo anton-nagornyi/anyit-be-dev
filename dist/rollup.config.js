08e69eb0316055cb9e2494a07d8b006063ae22cb
anton <anton.nagornyi@gmail.com>
2024-01-15 12:06:01 +0200

    fix: index.d.ts issues. Added more logging

--- dist/rollup.config.js
+++ dist/rollup.config.js
@@ -5,6 +5,17 @@ const typescript = require('rollup-plugin-typescript2');
 
 const currentLib = process.env.INIT_CWD;
 
+console.log('@@@@@@@@@@', process.env);
+
+console.log('-------', {
+  include: [join(currentLib, 'src')],
+  compilerOptions: {
+    declarationDir: join(currentLib, 'dist/@types'),
+    outDir: join(currentLib, 'dist')
+  }
+});
+
+
 module.exports = [
   {
     input: './index.ts',
